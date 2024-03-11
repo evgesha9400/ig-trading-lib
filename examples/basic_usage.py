31dbea05fc6bbb81deca7c7f451809596af59cfa
Evgeny Aleshin <aleshiner@gmail.com>
2024-03-11 19:49:50 +0000

    updated usage example

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,12 +1,24 @@
+import os
 from ig_trading_lib.authentication import AuthenticationService
+from ig_trading_lib.trading import get_open_positions
 
 
 if __name__ == "__main__":
+    api_key = os.environ.get("IG_API_KEY") or "your_api_key"
+    account_identifier = os.environ.get("IG_ACCOUNT_IDENTIFIER") or "your_account_identifier"
+    account_password = os.environ.get("IG_ACCOUNT_PASSWORD") or "your_account_password"
+    base_url = os.environ.get("IG_BASE_URL") or "https://demo-api.ig.com"
+
     auth_service = AuthenticationService(
-        api_key="example_api_key",
-        account_identifier="example_account",
-        account_password="example_password",
-        base_url="https://demo-api.ig.com",
+        api_key=api_key,
+        account_identifier=account_identifier,
+        account_password=account_password,
+        base_url=base_url,
     )
     auth_response = auth_service.authenticate()
-    print(auth_response.tokens)
+
+    open_positions = get_open_positions(tokens=auth_response.tokens, base_url=base_url)
+
+
+
+
