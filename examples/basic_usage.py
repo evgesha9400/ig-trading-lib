eb950a3a7d5ce9e42594847a7fe23c28b1469f24
Evgeny Aleshin <aleshiner@gmail.com>
2024-03-14 02:30:55 +0000

    updated basic usage examples

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,9 +1,14 @@
 import os
+import pprint
+
+from authentication.cache import InMemoryCache
 from ig_trading_lib.authentication import AuthenticationService
-from ig_trading_lib.trading import get_open_positions
+from ig_trading_lib.trading.positions import PositionService, CreatePosition
 
 
 if __name__ == "__main__":
+    """This example demonstrates how to use the PositionService to open a position and retrieve open positions."""
+
     api_key = os.environ.get("IG_API_KEY") or "your_api_key"
     account_identifier = os.environ.get("IG_ACCOUNT_IDENTIFIER") or "your_account_identifier"
     account_password = os.environ.get("IG_ACCOUNT_PASSWORD") or "your_account_password"
@@ -14,10 +19,37 @@ if __name__ == "__main__":
         account_identifier=account_identifier,
         account_password=account_password,
         base_url=base_url,
+        cache=InMemoryCache(),
     )
     auth_response = auth_service.authenticate()
 
-    open_positions = get_open_positions(tokens=auth_response.tokens, base_url=base_url)
+    position_service = PositionService(
+        api_key=api_key,
+        tokens=auth_response.tokens,
+        base_url=base_url
+    )
+
+    create = CreatePosition.model_validate(
+        {
+            "currencyCode": "USD",
+            "direction": "BUY",
+            "epic": "CS.D.GBPUSD.TODAY.IP",
+            "expiry": "DFB",
+            "forceOpen": True,
+            "guaranteedStop": False,
+            "orderType": "MARKET",
+            "size": 1,
+            "timeInForce": "EXECUTE_AND_ELIMINATE",
+            "trailingStop": False,
+        }
+    )
+    deal_reference = position_service.create_position(create=create)
+    pprint.pprint(deal_reference)
+
+    open_positions = position_service.get_open_positions()
+    pprint.pprint(open_positions.model_dump())
+
+
 
 
 
